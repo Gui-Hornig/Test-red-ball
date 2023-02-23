7a8f6c5bb7fe9bcd60019f3d4ecfb74f58bef29b
Gui-hornig <guihornig@gmail.com>
2023-02-23 13:37:18 -0300

    correção de bugs, js, html.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-document.addEventListener("click", function(event){
-    var ball = document.createElement('div');
+document.addEventListener("click", function(event) {
+    var ball = document.createElement("div");
     ball.classList.add("ball");
     ball.style.top = event.clientY - 25 + "px";
-    ball.style.left = event.clientX - 24 + "px";
+    ball.style.left = event.clientX - 25 + "px";
     document.body.appendChild(ball);
-    setTimeout(function(){
-        document.body.remove(ball);
+    setTimeout(function() {
+        document.body.removeChild(ball);
     }, 1000);
-})
+});
