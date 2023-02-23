f63c5aa0031abe338b2d6b8a4a4a528e72c7a724
Gui-hornig <guihornig@gmail.com>
2023-02-23 13:10:57 -0300

    Criado esqueleto JS

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("click", function(event){
+    var ball = document.createElement('div');
+    ball.classList.add("ball");
+    ball.style.top = event.clientY - 25 + "px";
+    ball.style.left = event.clientX - 24 + "px";
+    document.body.appendChild(ball);
+    setTimeout(function(){
+        document.body.remove(ball);
+    }, 1000),
+})
